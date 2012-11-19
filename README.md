4654f163da40f59366116888bb179fc5bbbd3983
andruxasazonov <Andrey@solaris.(none)>
2012-11-19 21:31:00 +0600

    bug with hanging fixed (via signals)

--- README.md
+++ README.md
@@ -2,24 +2,20 @@ termpy
 ======
 
 TermPy - simple terminal for *nix systems, based on vte, gtk and python.
-To try it please download next two files:
-1. show,
-2. term.py
+To try it please download next two files: show, term.py
 
-Do chmod +x on these two files.
-You can add show in your $PATH.
+Do chmod +x on these two files. You can add show in your $PATH.
 
-Usage: launch ./term.py. 
+Usage: launch ./term.py.
 Then try:
-./show your_image.jpg
+'./show your_image.jpg'
 or
-cat your_image.jpg | ./show
-
-NOTE: This is ALPHA version of TermPy!
-If 'show' hangs up - click Enter key once. This is known bug, sorry. Not yet fixed. 
+'cat your_image.png | ./show'
 
 With best regards,
 Sazonov Andrey
+
+P.S. THANKS TO AUTHORS OF GTK, VTE AND PYTHON
 
 Screenshots:
 ![Screenshot 1](https://github.com/downloads/AndruxaSazonov/termpy/Screenshot2.png)
--- term.py
+++ term.py
@@ -2,6 +2,7 @@
 import gtk
 import vte
 import os
+import signal
 
 class DrawableVTE(vte.Terminal, gtk.DrawingArea):
     def __init__(self):
@@ -15,13 +16,18 @@ class MainWindow(gtk.Window):
         self.vte = DrawableVTE()
         self.vte.connect("child-exited", self.command_executed)
         self.vte.connect("eof", self.command_executed)
-        self.connect_after("key-release-event", self.keypressed)
+        self.connect("key-press-event", self.clear)
         self.vte.connect_after("expose-event", self.do_paint)
         self.add(self.vte)
         self.pid = self.vte.fork_command(command = 'bash')
         self.pipe_name = "/tmp/pipe_show_%s" % str(self.pid)
+        self.initial = "/tmp/show_gppid_%s"  % str(self.pid)
+        with open(self.initial, "w+") as f:
+             f.write(str(os.getpid()))
         self.loader = gtk.gdk.PixbufLoader()
         self.pixbuf = None
+        self.last_row_added = False
+        column, self.last_row = self.vte.get_cursor_position()
 
     def main_quit(self, widget, window):
        try:
@@ -29,7 +35,14 @@ class MainWindow(gtk.Window):
        except:
           pass
        self.remove_pipe()
+       self.remove_gpid()
        gtk.main_quit()
+
+    def remove_gpid(self):
+      try:
+        os.unlink(self.initial)
+      except:
+        pass
 
     def remove_pipe(self):
       try:
@@ -37,32 +50,45 @@ class MainWindow(gtk.Window):
       except:
         pass
 
-    def keypressed(self, widget, event):
+    def clear(self, widget, event):
+       realized = False
+       if self.pixbuf is not None:
+          column, row = self.vte.get_cursor_position()
+          if row > self.last_row:
+             self.pixbuf = None
+             self.vte.realize()
+             realized = True
+       if event.keyval == 65293: # enter key
+          self.pixbuf = None
+          if not realized: self.vte.realize()
+          self.last_row += 1
+       return False
+
+    def preprocess_show(self):
         self.pixbuf = None
-        if event.keyval == 65293: # enter key
-             if not os.path.exists(self.pipe_name):
-                return False
-             try:
-               pipe = open(self.pipe_name, "rb")
-             except:
-               self.remove_pipe()
-               return False
-             if not pipe:
-                self.remove_pipe()
-                return False
-             for line in pipe:
-                self.loader.write(line)
-             do = False
-             try:
-                self.loader.close()
-                do = True
-             except:
-                pass
-             if not pipe.closed: pipe.close()
-             self.remove_pipe()
-             if do: self.do_show()
-             self.loader = gtk.gdk.PixbufLoader()
-        return False
+        if not os.path.exists(self.pipe_name):
+           return False
+        try:
+           pipe = open(self.pipe_name, "rb")
+        except:
+           self.remove_pipe()
+           return False
+        if not pipe:
+           self.remove_pipe()
+           return False
+        for line in pipe:
+           self.loader.write(line)
+        do = False
+        try:
+            self.loader.close()
+            do = True
+        except:
+             pass
+        if not pipe.closed: pipe.close()
+        self.remove_pipe()
+        if do: self.do_show()
+        self.loader = gtk.gdk.PixbufLoader()
+        return True
 
     def do_show(self):
         x, y, width, height = self.vte.get_allocation()
@@ -101,8 +127,12 @@ class MainWindow(gtk.Window):
        return False
 
     def command_executed(self, terminal):
-        gtk.main_quit()
+        self.main_quit(self.vte, self.vte.window)
+
+    def SIGUSR_handler(self, signum, frame):
+        self.preprocess_show()
 
 main_win = MainWindow()
+signal.signal(signal.SIGUSR1, main_win.SIGUSR_handler)
 main_win.show_all()
 gtk.main()
